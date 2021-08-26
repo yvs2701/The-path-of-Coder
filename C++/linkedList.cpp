aa8dd600a123a129848d68a7a33c7d18aa3bf535
Yashvardhan Singh <79207707+yvs2701@users.noreply.github.com>
2021-08-26 16:29:28 +0530

    some more null checks

--- C++/linkedList.cpp
+++ C++/linkedList.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
-#include <string> //just to use the string once in the program
+#include <string> // just to use the string once in the program
 using namespace std;
-/*nullptr is always a pointer type. 0 (aka. C's NULL bridged over into C++) could cause ambiguity in overloaded function resolution
-so we will use a nullptr*/
+/* nullptr is always a pointer type. 0 (aka. C's NULL bridged over into C++) could cause ambiguity in overloaded function resolution
+so we will use a nullptr */
 
 class linkedList
 {
 public:               // since everygting is public we could've used a struct
     int data;         // stores the value of that link/node
-    linkedList *next; //stores the memory address of the next linkedList in the list
+    linkedList *next; // stores the memory address of the next linkedList in the list
 
     linkedList()
     {
@@ -27,26 +27,30 @@ public:               // since everygting is public we could've used a struct
     }
 };
 
-void print(linkedList *head, string sep = " ") //here we took head by value as we don't need to modify list in this fn
+void print(linkedList *head, string sep = " ") // here we took head by value as we don't need to modify list in this fn
 {
-    cout << head->data;
-    head = head->next;
     if (head == nullptr)
         return;
-    while (head->next != nullptr)
+    cout << head->data;
+    if (head->next == nullptr)
+        return;
+    head = head->next;
+    while (head != nullptr)
     {
         cout << sep << head->data; // separator between two couts
         head = head->next;         // ptr is updated to pointer of the next node (which points to next to next node)
     }
-    cout << sep << head->data << endl;
+    cout << endl;
 }
 
-int len(linkedList *head){
+int len(linkedList *head)
+{
     int length = 0;
     if (head == nullptr)
         return length;
     linkedList *ptr = head;
-    while(head->next != nullptr){
+    while (head->next != nullptr)
+    {
         length++;
         head = head->next;
     }
@@ -57,17 +61,24 @@ int len(linkedList *head){
 
 void insert(linkedList *&head, int index, int d)
 {
+    if (head == nullptr) // when we have an empty list
+    {
+        if (index == 0)
+            head = new linkedList(d);
+        else
+            cout << "indexOutOfBounds\n";
+        return;
+    }
     linkedList *ptr = head;
-    index -= 1; //decreasing one index as we have to insert at this index not after this index
+    index -= 1; // decreasing one index as we have to insert at this index not after this index
     while (index--)
     {
         if (ptr->next != nullptr)
-            ptr = ptr->next; //navigating to the link/node at that index
+            ptr = ptr->next; // navigating to the link/node at that index
         else
         {
             cout << "indexOutOfBounds\n";
-            // throw "indexOutOfBounds";
-            return; // instead of this you can use a check variable which will decide shouldthe code after this loop execute or not
+            return; // instead of this you can use a check variable which will decide should the code after this loop execute or not
         }
     }
 
@@ -75,58 +86,83 @@ void insert(linkedList *&head, int index, int d)
     newNode->next = ptr->next;               // (since we were one index before what is given to us)
     ptr->next = newNode;
 }
-void append(linkedList *&head, int d) //insert at the end
+void append(linkedList *&head, int d) // insert at the end
 {
-    if (head == NULL) //when we have an empty list
+    if (head == nullptr) // when we have an empty list
     {
-        head = new linkedList(d); //then we directly assign head to this new link/node
-        return;                   //return after doing the work
+        head = new linkedList(d); // then we directly assign head to this new link/node
+        return;                   // return after doing the work
     }
 
-    linkedList *ptr = head; //initialising another pointer variable (we dont want to mess up with original memory address of head)
+    linkedList *ptr = head; // initialising another pointer variable (we dont want to mess up with original memory address of head)
     while (ptr->next != nullptr)
     {
-        ptr = ptr->next; //finding the tail of the linked list
+        ptr = ptr->next; // finding the tail of the linked list
     }
 
-    ptr->next = new linkedList(d); //appended (inserted at the end)
+    ptr->next = new linkedList(d); // appended (inserted at the end)
     // newNode->next =  NULL; it is already  NULL coz we specified that in our contructor
 }
-void prepend(linkedList *&head, int d) //insert at head
+void prepend(linkedList *&head, int d) // insert at head
 {
+    if (head == nullptr) // when we have an empty list
+    {
+        head = new linkedList(d); // then we directly assign head to this new link/node
+        return;                   // return after doing the work
+    }
     linkedList *newNode = new linkedList(d);
     newNode->next = head;
-    head = newNode; //since we called by reference this change will reflect everywhere
+    head = newNode; // since we called by reference this change will reflect everywhere
 }
 
 void delHead(linkedList *&head)
 {
+    if (head == nullptr) // list has no elements
+    {
+        cout << "nothingToDelete\n";
+        return;
+    }
+    if (head->next == nullptr)
+    {
+        linkedList *toDelete = head;
+        head = nullptr;
+        delete toDelete;
+        return;
+    }
     linkedList *toDelete = head;
     head = head->next;
-    delete toDelete; //deleting the waste memory
+    delete toDelete; // deleting the waste memory
 }
 void delTail(linkedList *&head)
 {
+    if (head == nullptr) // list has no elements
+    {
+        cout << "nothingToDelete\n";
+        return;
+    }
     linkedList *ptr = head;
     if (ptr == nullptr)
         return;
     if (ptr->next == nullptr)
         cout << "noElementsAhead\n";
-    while (ptr->next->next != nullptr) //one node before the tail
+    while (ptr->next->next != nullptr) // one node before the tail
     {
         ptr = ptr->next;
     }
     linkedList *toDelete = ptr->next;
     ptr->next = nullptr;
-    delete toDelete; //deleting the waste node
+    delete toDelete; // deleting the waste node
 }
 void del(linkedList *&head, int index)
 {
     if (index >= len(head))
-        cout<<"indexOutOfBounds\n";
-    if (head == nullptr)            //list has no elements
-        return;                     //do nothing
-    else if (head->next == nullptr) //we are at head with no elements after
+        cout << "indexOutOfBounds\n";
+    if (head == nullptr) // list has no elements
+    {
+        cout << "nothingToDelete\n";
+        return;
+    }
+    else if (head->next == nullptr) // we are at head with no elements after
     {
         delHead(head);
         return;
@@ -138,7 +174,7 @@ void del(linkedList *&head, int index)
         delHead(head);
         return;
     }
-    
+
     index -= 1; // first getting the node just before the one to delete
     while (index--)
         ptr = ptr->next;
@@ -151,17 +187,17 @@ void del(linkedList *&head, int index)
         return;
     }
 
-    linkedList *toDelete = ptr->next; //store the value at the next node (whose address was given)
+    linkedList *toDelete = ptr->next; // store the value at the next node (whose address was given)
     ptr->next = ptr->next->next;
-    delete toDelete; //deleting the waste node from the memory
+    delete toDelete; // deleting the waste node from the memory
 }
 
-void deleteMafterN(linkedList* head, int n, int m)
+void deleteMafterN(linkedList *head, int n, int m)
 {
     /* deleted n nodes after every m nodes */
     int counter = 1;
     // head has to be counted anyways
-    while(head != nullptr)
+    while (head != nullptr)
     {
         if (counter == m)
         {
@@ -176,7 +212,9 @@ void deleteMafterN(linkedList* head, int n, int m)
             counter = 1;
             // since the way we are counting head will shift one step ahead already
             // when we realise that counter = m !!!
-        } else counter++;
+        }
+        else
+            counter++;
         head = head->next;
     }
 }
@@ -200,7 +238,7 @@ void reverse(linkedList *&head) // reverses the linkedlist in O(n)
     linkedList *prev = nullptr; // use NULL for earlier versions of C++
     linkedList *current;
 
-    while(head != nullptr)
+    while (head != nullptr)
     {
         current = head;
 
@@ -257,8 +295,8 @@ bool isPalindrome(linkedList *head) // checks if the list is palindrome
 
     // if the length was odd then rightHalf shall reach the NULL
     // and head will be at the middle (before splitting) of the list
-    
-    linkedList *ptr = rightHalf; 
+
+    linkedList *ptr = rightHalf;
     // we have to preserve rightHalf to again correct the list as it was before reversing the right half !
     while (ptr != nullptr)
     {
@@ -274,7 +312,7 @@ bool isPalindrome(linkedList *head) // checks if the list is palindrome
     return true;
 }
 
-void sort_list(linkedList* head)
+void sort_list(linkedList *head)
 {
     linkedList *ptr;
     int sortTill = len(head);
@@ -306,26 +344,28 @@ void sort_list(linkedList* head)
     }
 }
 
-linkedList* splitAlternate(linkedList *head)
+linkedList *splitAlternate(linkedList *head)
 {
     if (head == nullptr || head->next == nullptr)
         return nullptr;
 
-    linkedList* second_head = head->next;
-    linkedList* ptr = second_head; // for traversing
+    linkedList *second_head = head->next;
+    linkedList *ptr = second_head; // for traversing
 
-    while(head != nullptr && head->next != nullptr && ptr != nullptr && ptr->next != nullptr)
+    while (head != nullptr && head->next != nullptr && ptr != nullptr && ptr->next != nullptr)
     {
-        head->next = head->next->next; head = head->next;
+        head->next = head->next->next;
+        head = head->next;
         // head should be updated before the second head always !
-        ptr->next = ptr->next->next; ptr = ptr->next;
+        ptr->next = ptr->next->next;
+        ptr = ptr->next;
     }
-    while(head != nullptr && head->next != nullptr)
+    while (head != nullptr && head->next != nullptr)
     {
         head->next = head->next->next;
         head = head->next;
     }
-    while(ptr != nullptr && ptr->next != nullptr)
+    while (ptr != nullptr && ptr->next != nullptr)
     {
         ptr->next = ptr->next->next;
         ptr = ptr->next;
@@ -333,12 +373,12 @@ linkedList* splitAlternate(linkedList *head)
     return second_head;
 }
 
-void removeDup(linkedList* node)
+void removeDup(linkedList *node)
 {
-    while(node->next != nullptr)
+    while (node->next != nullptr)
     {
         linkedList *ptr = node;
-        while(true)
+        while (true)
         {
             if (ptr->next == nullptr)
                 break;
@@ -347,7 +387,7 @@ void removeDup(linkedList* node)
             // since this is passed to be the head of the list it would be treated as index 0 in del function
             else
             {
-                // very important to use else otheriwse if ptr moves ahead and 
+                // very important to use else otheriwse if ptr moves ahead and
                 // say there were 3 repeating elements in a row then we will miss one
                 ptr = ptr->next;
             }
@@ -371,10 +411,11 @@ int main()
     prepend(ll, 5);   // 5->1->3->4->0->2->-1->null
     print(ll, " -> ");
     cout << "6 is in the list? " << indexOf(ll, 6) << "\n"; // earlier I made a search function but doesn't matter indexOf will do the job too
-    cout << "2 is at the " << indexOf(ll, 2) << "th index" << "\n"; //index = 5
+    cout << "2 is at the " << indexOf(ll, 2) << "th index"
+         << "\n"; //index = 5
     del(ll, 0);   //delete at 0th index i.e. 5 (head)
     del(ll, 3);   //delete at 3rd index i.e. 0
-    cout<<"Length of this list is: "<<len(ll)<<"\n";
+    cout << "Length of this list is: " << len(ll) << "\n";
     print(ll, " => ");
     reverse(ll); // reverse a list
     print(ll, " <- ");
