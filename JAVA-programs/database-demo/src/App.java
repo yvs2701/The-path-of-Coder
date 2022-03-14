046ba3be20864af7fe42cbfcf9cc983229e922af
yvs2701 <yashvardhan.ys86@gmail.com>
2022-03-14 16:14:19 +0530

    added more functions

--- JAVA-programs/database-demo/src/App.java
+++ JAVA-programs/database-demo/src/App.java
@@ -8,21 +8,24 @@ public class App {
         try {
 
             Class.forName("com.mysql.cj.jdbc.Driver");
-            // dont use com.mysql.jdbc.Driver to avoid the following warning:
-            // Loading class `com.mysql.jdbc.Driver'.
-            // This is deprecated. The new driver class is `com.mysql.cj.jdbc.Driver'.
-            // The driver is automatically registered via the SPI and manual loading
-            // of the driver class is generally unnecessary.
-
-            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/yashv", "root", "root");
-            // yashv is the name of the db
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/CitiesDB", "root", "root");
 
             Statement stmt = con.createStatement();
-            ResultSet rs = stmt.executeQuery("select * from Student");
+            // create table
+            stmt.executeUpdate("CREATE TABLE Cities(name varchar(255) not null, population int not null, districts int not null, state varchar(255) not null)");
+            
+            // insert records
+            stmt.executeUpdate("INSERT INTO Cities VALUES('City_1', 188012, 18, 'State_1')");
+            stmt.executeUpdate("INSERT INTO Cities VALUES('City_2', 200015, 24, 'State_2')");
+            stmt.executeUpdate("INSERT INTO Cities VALUES('City_3', 198970, 20, 'State_3')");
+            stmt.executeUpdate("INSERT INTO Cities VALUES('City_4', 300112, 30, 'State_4')");
+
+            // cities with population < 200000
+            ResultSet rs = stmt.executeQuery("select population, districts, state from Cities where population < 200000");
 
             while (rs.next()) {
-                // second column was int, 1st and 3rd one were varchar
-                System.out.println(rs.getInt(2) + "  \'" + rs.getString(1) + "\'  " + rs.getInt(3));
+                // 1stand 3rd column (population and state) is string districts is int
+                System.out.println(rs.getString(1) + "  \'" + rs.getInt(2) + "\'  " + rs.getString(3));
             }
             con.close();
         } catch (Exception e) {
