e92268203cbab7b0ad114c3af37fae953542cb4b
yvs2701 <yashvardhan.ys86@gmail.com>
2022-04-24 08:38:04 +0530

    rollback:detect only 2 classes, added:config files

--- Python/face-mask-detection/main.py
+++ Python/face-mask-detection/main.py
@@ -3,29 +3,24 @@ import cv2
 import numpy as np
 
 if __name__ == '__main__':
-    with_mask = np.load('test/data/with_mask.npy')
+    with_mask = np.load('data/with_mask.npy')
     row_with_mask, a, b, c = with_mask.shape
     with_mask = with_mask.reshape(row_with_mask, a*b*c)
 
-    without_mask = np.load('test/data/without_mask.npy')
+    without_mask = np.load('data/without_mask.npy')
     row_without_mask, a, b, c = without_mask.shape
     without_mask = without_mask.reshape(row_without_mask, a*b*c)
 
-    wrong_mask = np.load('test/data/wrong_mask.npy')
-    row_wrong_mask, a, b, c = wrong_mask.shape
-    wrong_mask = wrong_mask.reshape(row_wrong_mask, a*b*c)
-
-    X = np.r_[with_mask, without_mask, wrong_mask] # merge them row wise
+    X = np.r_[with_mask, without_mask] # merge them row wise
     labels = np.zeros(X.shape[0])
     # divide the array in with mask (0) and without mask
     labels[row_with_mask:] = 1 # all the rows from from last row_with_mask to end is rows without mask
-    labels[row_with_mask + row_wrong_mask:] = 2 # all the rows from from last row_with_mask to end is rows under the nose
-    rectangle_label = {0: 'Wearing Mask', 1: 'No Mask', 2: 'Under the Nose'}
+    rectangle_label = {0: 'Wearing Mask', 1: 'No Mask'}
 
     haar_data = cv2.CascadeClassifier('data/haarcascade_frontal_face_default.xml')
     capture = cv2.VideoCapture(0)
 
-    svm = joblib.load('test/model/svm_model.pkl')
+    svm = joblib.load('model/svm_model.pkl')
 
     print('press \'q\' to exit')
     # detecting mask from video stream
@@ -45,8 +40,6 @@ if __name__ == '__main__':
                     cv2.rectangle(img, (x,y), (x+w,y+h), (0, 255, 0), 4) # green rectangle (wearing mask)
                 elif pred == 1:
                     cv2.rectangle(img, (x,y), (x+w,y+h), (0, 0, 255), 4) # red rectangle (no mask)
-                elif pred == 2:
-                    cv2.rectangle(img, (x,y), (x+w,y+h), (0, 255, 255), 4) # yellow rectangle (no mask)
                 # out a label on this rectangle
                 cv2.putText(img=img, text=rectangle_label[pred], org=(x,y), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1, color=(255,255,255), thickness=2)
 
--- Python/face-mask-detection/train_model.py
+++ Python/face-mask-detection/train_model.py
@@ -5,24 +5,19 @@ from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 
 if __name__ == "__main__":
-    with_mask = np.load('test/data/with_mask.npy')
+    with_mask = np.load('data/with_mask.npy')
     row_with_mask, a, b, c = with_mask.shape
     with_mask = with_mask.reshape(row_with_mask, a*b*c)
 
-    without_mask = np.load('test/data/without_mask.npy')
+    without_mask = np.load('data/without_mask.npy')
     row_without_mask, a, b, c = without_mask.shape
     without_mask = without_mask.reshape(row_without_mask, a*b*c)
 
-    wrong_mask = np.load('test/data/wrong_mask.npy')
-    row_wrong_mask, a, b, c = wrong_mask.shape
-    wrong_mask = wrong_mask.reshape(row_wrong_mask, a*b*c)
-
-    X = np.r_[with_mask, without_mask, wrong_mask] # merge them row wise
+    X = np.r_[with_mask, without_mask] # merge them row wise
     labels = np.zeros(X.shape[0])
     # divide the array in with mask (0) and without mask
     labels[row_with_mask:] = 1 # all the rows from from last row_with_mask to end is rows without mask
-    labels[row_with_mask + row_wrong_mask:] = 2 # all the rows from from last row_with_mask to end is rows under the nose
-    rectangle_label = {0: 'Wearing Mask', 1: 'No Mask', 2: 'Under the Nose'}
+    rectangle_label = {0: 'Wearing Mask', 1: 'No Mask'}
 
     x_train, x_test, y_train, y_test = train_test_split(X, labels, test_size=0.2) # 20% data for testing
 
@@ -33,6 +28,6 @@ if __name__ == "__main__":
     accuracy = accuracy_score(y_test, y_pred)
     print(accuracy)
     if accuracy >= 0.90:
-        joblib.dump(svm, 'test/model/svm_model.pkl') # save model if it is accurate enough
+        joblib.dump(svm, 'model/svm_model.pkl') # save model if it is accurate enough
     else:
         raise Exception('Accuracy was less than 90%\nModel was not saved, try again :(')
